377743bb04ecc2642bc1be7e1444403e2dc9987a
Hamza Hassan <hassanhamza930@gmail.com>
2022-04-16 04:24:05 +0500

    Basic Page Setup

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
       
       <Header/>
       
-      <div className="flex flex-row h-screen w-full bg-white">
+      <div className="flex flex-col md:flex-row w-full bg-white ">
  
-        <div className="h-full w-2/4 mt-10 text-black/80  p-10 text-3xl font-bold">
+        <div className="h-72 md:w-3/4 w-full mt-10 text-black/80  p-5 text-3xl font-regular text-5xl">
           Changing the way you interact with your favourite celebrities.
         </div>
 
-        <div>
+        <div style={{backgroundImage:"url('https://i.scdn.co/image/ab6761610000e5eb276369b9195d6f67470fd51a')"}} className="h-96 self-center mb-10 w-3/4 md:w-1/4 bg-cover bg-center bg-red-500 rounded-md md:m-10">
 
         </div>
  
--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,14 @@
 export default function Header(){
-   return <div className=" flex flex-col md:flex-row justify-start md:justify-between items-start md:items-start p-10">
+   return <div className=" flex flex-col md:flex-row  md:justify-between items-start md:items-center p-5">
      
-        <div className=" text-5xl md:text-6xl font-bold text-start flex flex-col">
+        <div className=" text-4xl md:text-5xl font-bold text-start flex flex-col">
           <div>
             Stardom
           </div>
-          <div className="text-xl md:text-2xl font-normal mt-4">
-            Celebrities are just a message away.
-          </div>  
+
         </div>
 
-        <div className="flex mt-5 flex-wrap justify-start items-center gap-5  md:gap-10 text-md md:text-2xl font-bold ">
+        <div className="flex mt-5 md:mt-0 flex-wrap justify-center items-center gap-5  md:gap-10 text-md md:text-xl font-bolds ">
           <button className="">Download</button>
           <button className="">About</button>
           <button className="">Contact</button>
