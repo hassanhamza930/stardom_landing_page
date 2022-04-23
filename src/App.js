699a2071367397a7f35638662f6408379635c221
Hamza Hassan <hassanhamza930@gmail.com>
2022-04-23 19:44:29 +0500

    basi changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       
       <div className="flex flex-col md:flex-row w-full bg-white ">
  
-        <div className="h-72 md:w-3/4 w-full mt-10 text-black/80  p-5 text-3xl font-regular text-5xl">
+        <div className="h-64 md:w-3/4 w-full mt-10 text-black/80  p-5 text-5xl font-regular ">
           Changing the way you interact with your favourite celebrities.
         </div>
 
-        <div style={{backgroundImage:"url('https://i.scdn.co/image/ab6761610000e5eb276369b9195d6f67470fd51a')"}} className="h-96 self-center mb-10 w-3/4 md:w-1/4 bg-cover bg-center bg-red-500 rounded-md md:m-10">
+        <div style={{backgroundImage:"url('https://i.scdn.co/image/ab6761610000e5eb276369b9195d6f67470fd51a')"}} className="h-96 self-start ml-5 mb-10 w-3/4 md:w-1/4 bg-cover bg-center bg-red-500 rounded-md md:m-10">
 
         </div>
  
